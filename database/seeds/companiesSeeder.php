571526fd835776f6224e4eaa3220e3804a590a79
Toshi-Yano <62419040+Toshi-Yano@users.noreply.github.com>
2020-07-09 03:07:40 +0900

    create seeds_data

--- database/seeds/companiesSeeder.php
+++ database/seeds/companiesSeeder.php
@@ -11,17 +11,23 @@ class companiesSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('companies')->insert([
-            'company_name' => 'AAA株式会社',
-            'company_note' => "第一志望", 
+        DB::table("companies")->insert([
+            "company_name" => "AAA株式会社",
+            "company_note" => "第一志望", 
+            "created_at" => new DateTime(),
+            "updated_at" => new DateTime(),
         ]);
-        DB::table('companies')->insert([
-            'company_name' => 'BBB株式会社',
-            'company_note' => "第二志望", 
+        DB::table("companies")->insert([
+            "company_name" => "BBB株式会社",
+            "company_note" => "第二志望", 
+            "created_at" => new DateTime(),
+            "updated_at" => new DateTime(),
         ]);
-        DB::table('companies')->insert([
-            'company_name' => 'CCC株式会社',
-            'company_note' => "第三志望", 
+        DB::table("companies")->insert([
+            "company_name" => "CCC株式会社",
+            "company_note" => "第三志望",
+            "created_at" => new DateTime(),
+            "updated_at" => new DateTime(),
         ]);
     }
 }
