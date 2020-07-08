8422935e58e794c70ab9e51c6aab4f282c46fcc7
Toshi-Yano <62419040+Toshi-Yano@users.noreply.github.com>
2020-07-09 02:12:32 +0900

    create company_controller/model

---
+++ database/migrations/2020_07_08_165252_create_companies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('company_name', 100);
+            $table->string('company_note', 1000);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
