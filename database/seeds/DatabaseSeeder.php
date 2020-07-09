e33619f3bcba34f1eacae5ca7c3828396ff3d176
Toshi-Yano <62419040+Toshi-Yano@users.noreply.github.com>
2020-07-09 13:26:29 +0900

    edit migration

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(StageTableSeeder::class);
+        $this->call(RouteTableSeeder::class);
+        $this->call(companiesSeeder::class);
+        $this->call(InterviewTableSeeder::class);
     }
 }
