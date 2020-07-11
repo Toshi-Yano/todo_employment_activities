2b0422c2cb0825a59f0abf08e0bdae526776eacf
Toshi-Yano <62419040+Toshi-Yano@users.noreply.github.com>
2020-07-12 07:32:09 +0900

    create home.index

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'HomeController@index')->name('home');
 
-Route::get('/', 'InterviewController@index') ->name("interviews.index");
+Route::get('/interviews/index', 'InterviewController@index') ->name("interviews.index");
 Route::get('/companies/{id}/interviews/create', 'InterviewController@showCreateForm')->name('interviews.create');
 Route::post('/companies/{id}/interviews/create', 'InterviewController@create');
 Route::get('/companies/{id}/interviews/{interview_id}/edit', 'InterviewController@showEditForm')->name('interviews.edit');
